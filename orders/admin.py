d735ec2653de3a1f41591b9a1a55254d77f1641d
danialgagne <gagne.danial@gmail.com>
2020-03-18 23:21:25 -0500

    set up menu models and admin for users to add menu items

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Item, Topping
+
+
+class ItemInline(admin.StackedInline):
+    model = Item
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [ItemInline]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Item)
+admin.site.register(Topping)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+    small_price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+    large_price = models.DecimalField(max_digits=5, decimal_places=2)
+    toppings_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.category} - {self.name}"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=64, null=False)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="toppings",
+    )
+
+    def __str__(self):
+        return f"{self.category} - {self.name}"
