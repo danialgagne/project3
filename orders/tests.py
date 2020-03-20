57691c47c01f306d35f663a0ff851c86e5bfed44
danialgagne <gagne.danial@gmail.com>
2020-03-19 20:58:22 -0500

    add first unit test for Category model

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Category
+
+
+@pytest.mark.django_db
+class TestCategory:
+
+    def test_create_category(self):
+        """Test adding records to the Category Model"""
+        c1 = Category.objects.create(name="Pizza")
+        c2 = Category.objects.create(name="Salads")
+        categories = Category.objects.all()
+        assert categories.count() == 2
+
