3aa8650a48cd8de5d5dc97c5e6bb6f4acbaf7fe5
danialgagne <gagne.danial@gmail.com>
2020-03-21 10:39:32 -0500

    add test for max price

--- orders/tests/test_models.py
+++ orders/tests/test_models.py
@@ -1,27 +1,29 @@
 import pytest
+from decimal import InvalidOperation
 
 from orders.models import Category, Item, Topping
 
 
 pytestmark = pytest.mark.django_db
 
+
 class TestCategory:
 
     def test_create_category(self):
         """Test adding records to the Category model"""
+
         c1 = Category.objects.create(name="Pizza")
         c2 = Category.objects.create(name="Salads")
         categories = Category.objects.all()
         assert categories.count() == 2
 
 
-
 class TestItem:
 
     def test_create_item(self):
         """Test adding records to the Item model"""
-        category = Category.objects.create(name="Pizza")
 
+        category = Category.objects.create(name="Pizza")
         item = Item.objects.create(
             name="1 Topping",
             category=category,
@@ -31,14 +33,25 @@ class TestItem:
         items = Item.objects.all()
         assert items.count() == 1
 
+    def test_digits_raises(self):
+        """Prices with more than 5 digits should raise an error"""
+
+        category = Category.objects.create(name="Pizza")
+        with pytest.raises(InvalidOperation):
+            Item.objects.create(
+                name="1 Topping",
+                category=category,
+                large_price= 1000.00,
+                toppings_allowed=True
+            )
 
 
 class TestTopping:
 
     def test_create_topping(self):
         """Test adding records to the Topping model"""
+        
         category = Category.objects.create(name="Pizza")
-
         t1 = Topping.objects.create(name="Pepperoni", category=category)
         t2 = Topping.objects.create(name="Sausage", category=category)
         t3 = Topping.objects.create(name="Mushrooms", category=category)
