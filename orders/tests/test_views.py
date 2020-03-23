06a115bf5dcb57085f9b48357e67f6d7a48f303c
danialgagne <gagne.danial@gmail.com>
2020-03-22 20:50:35 -0500

    add view tests

--- orders/tests/test_views.py
+++ orders/tests/test_views.py
@@ -1,9 +1,16 @@
 from django.test import Client
+from django.urls import resolve
+
+from orders.views import index, sign_up
 
 
 class TestIndex:
     """all tests relating to the index route"""
 
+    def test_root_resolves_to_index_view(self):
+        found = resolve("/")
+        assert found.func == index
+    
     def test_index_status_code(self):
         """index route returns status code 200"""
 
@@ -17,6 +24,10 @@ class TestIndex:
         c = Client()
         response = c.get("/")
         assert 'orders/index.html' in (t.name for t in response.templates)
+
+    def test_sign_up_resolves_to_sign_up_view(self):
+        found = resolve("/sign_up")
+        assert found.func == sign_up
 
     def test_get_sign_up_status_code(self):
         """register route redirects to index"""
