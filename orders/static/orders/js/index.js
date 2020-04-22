6814483ecf71a5e81272a82276b4a45869722d1c
danialgagne <gagne.danial@gmail.com>
2020-04-21 21:06:56 -0500

    display cart icon when items in cart

--- orders/static/orders/js/index.js
+++ orders/static/orders/js/index.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     let cart = localStorage.getItem('cart') ?
         JSON.parse(localStorage.getItem('cart')) : []
+    
+    if (cart.length > 0) {
+        document.getElementById('cart-icon').style.display = 'initial'
+    }
 
     document.addEventListener('click', event => {
         const element = event.target;
@@ -9,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const item = create_cart_item(element)
             cart.push(item);
             localStorage.setItem('cart', JSON.stringify(cart));
+            document.getElementById('cart-icon').style.display = 'initial'
         }
 
         const reset = ['Log out', 'Sign in', 'Sign up']
