857c30faa3ed9b93c009bdd011664e36ca2ba197
danialgagne <gagne.danial@gmail.com>
2020-04-21 22:34:53 -0500

    display shopping cart items in ui

--- orders/static/orders/js/index.js
+++ orders/static/orders/js/index.js
@@ -3,23 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
         JSON.parse(localStorage.getItem('cart')) : []
     
     if (cart.length > 0) {
-        document.getElementById('cart-icon').style.display = 'initial'
+        document.getElementById('cart-icon').style.display = 'initial';
     }
 
     document.addEventListener('click', event => {
         const element = event.target;
 
         if (element.classList.contains('item')) {
-            const item = create_cart_item(element)
+            const item = create_cart_item(element);
             cart.push(item);
             localStorage.setItem('cart', JSON.stringify(cart));
-            document.getElementById('cart-icon').style.display = 'initial'
+            document.getElementById('cart-icon').style.display = 'initial';
+            add_cart_item(item)
         }
 
         const reset = ['Log out', 'Sign in', 'Sign up']
         if (reset.includes(element.textContent)) {
-            cart = []
-            localStorage.clear()
+            cart = [];
+            localStorage.clear();
         }
 
         if (element.id == 'cart-icon') {toggle_cart()}
@@ -43,4 +44,11 @@ function toggle_cart () {
     document.getElementById('shopping-cart').style.width == '' ?
         document.getElementById('shopping-cart').style.width = '20em' :
         document.getElementById('shopping-cart').style.width = ''
+}
+
+const cart_item_template = Handlebars.compile(document.getElementById('cart-item').innerHTML);
+
+function add_cart_item(contents) {
+    const item = cart_item_template(contents)
+    document.getElementById('shopping-cart').innerHTML += item
 }
