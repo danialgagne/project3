bda53bc3dacf6b933b29ae6ab47d8918a0d1405d
danialgagne <gagne.danial@gmail.com>
2020-04-25 15:51:45 -0500

    show modal when adding topping permitted item to cart

--- orders/static/orders/js/index.js
+++ orders/static/orders/js/index.js
@@ -9,8 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('click', event => {
         const element = event.target;
+        const modal = document.getElementById('toppings-modal');
 
         if (element.classList.contains('item')) {
+            if (element.closest('tr').dataset.toppings == "True" ) {
+                modal.style.display = 'block';
+            }
             const item = create_cart_item(element);
             cart.push(item);
             localStorage.setItem('cart', JSON.stringify(cart));
@@ -25,6 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         if (element.id == 'cart-icon') {toggle_cart()}
+        if (element == modal) {modal.style.display = 'none'}
     })
 })
 
@@ -52,4 +57,8 @@ const cart_item_template = Handlebars.compile(document.getElementById('cart-item
 function add_cart_item(contents) {
     const item = cart_item_template(contents)
     document.getElementById('shopping-cart').innerHTML += item
+}
+
+function add_toppings(toppings, quantity) {
+
 }
